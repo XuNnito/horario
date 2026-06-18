e30e20da68242c5869bd8917277bc475f00ce655
XuNnito <241104@241104@ib.upchiapas.edu.mx>
2026-06-18 00:35:05 -0600

    Actualizar URL de backend en los archivos index.html

--- app.py
+++ app.py
@@ -251,6 +251,7 @@ def add_cors_headers(response):  # type: ignore[override]
 						"http://127.0.0.1:5500",
 						"http://localhost:5500",
 						"http://127.0.0.1:5500",
+						"https://horario-sigma-nine.vercel.app/",
 				]
 
 		if origin:
